529819e66bdf5eb128a78df7eb78a00332908986
Michelle Dicks <michd@alumni.stanford.edu>
2017-10-20 21:10:49 -0700

    some spacing changes

--- sampleAVLTree.java
+++ sampleAVLTree.java
@@ -4,7 +4,7 @@
 //  property. Lookup, insertion, and deletion all take O(log n) time in both the average and worst cases, 
 //  where n is the number of nodes in the tree prior to the operation. Insertions and deletions may require 
 //  the tree to be rebalanced by one or more tree rotations. This program is based on the implementation
-//  by Mark Allen Weiss.  
+//  by Mark Allen Weiss.          
 
     /*
 
